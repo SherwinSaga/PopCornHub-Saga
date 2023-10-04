860978bae0ca10cd8498d42698b2e84f0b068c88
Nicole Castro <oblivionshikigami97@gmail.com>
2023-10-04 20:55:24 +0800

    Login

--- Authentication/models.py
+++ Authentication/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class User(models.Model):
     # Add any additional user attributes here
     # Mao ra guro ni sa pagka karon
-    username = models.CharField(max_length=40, primary_key=True, null=False)
-    password = models.CharField(max_length=40, null=False)
-    first_name = models.CharField(max_length=40, null=False)
-    last_name = models.CharField(max_length=40, null=False)
-    email = models.EmailField(unique=True, max_length=40,  null=False)
+    username = models.CharField(max_length=45, primary_key=True, null=False)
+    password = models.CharField(max_length=45, null=False)
+    first_name = models.CharField(max_length=45, null=False)
+    last_name = models.CharField(max_length=45, null=False)
+    email = models.EmailField(unique=True, max_length=45,  null=False)
