a8ffd9c7d299051b7ee534753dee62c38a26b5b6
Franz Genegobis <genegobis.franz@gmail.com>
2023-10-04 12:50:16 +0800

    Made some changes sa relationshit sa atong models. All is good naman pud.

--- SavedMovies/models.py
+++ SavedMovies/models.py
@@ -2,21 +2,20 @@
 
 from django.db import models
 
-from UserProfile.models import Profile
-from Authentication.models import User
+from Authentication.models import *
 from Movie.models import Movie
 
 
 class FavoriteMovies(models.Model):
     favoriteID = models.IntegerField(primary_key=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     movieID = models.ForeignKey(Movie, on_delete=models.CASCADE)
     pass
 
 
 class WatchList(models.Model):
     watchlistID = models.IntegerField(primary_key=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # movieID = Foreign key sa list of movies
     pass
 
--- UserProfile/models.py
+++ UserProfile/models.py
@@ -2,14 +2,11 @@ from django.db import models
 
 from Authentication.models import User
 from MovieReviews.models import Review
-from SavedMovies.models import FavoriteMovies
-from SavedMovies.models import WatchList
-from SavedMovies.models import Watched
+from SavedMovies.models import *
 
 
 # Create your models here.
 class Profile(models.Model):
-    # Kulang pa ang user model.
     userID = models.OneToOneField(User, on_delete=models.CASCADE)
     favoriteID = models.ForeignKey(FavoriteMovies, on_delete=models.CASCADE)
     watchlistID = models.ForeignKey(WatchList, on_delete=models.CASCADE)
