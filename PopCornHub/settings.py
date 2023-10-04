c1a509ab5288cb52aa04c4f79fe9195c1694dc1d
Jess Oliver Zoilo <zjessoliver@gmail.com>
2023-10-04 11:30:49 +0800

    added movie and moviereviews to settings.py

--- PopCornHub/settings.py
+++ PopCornHub/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Authentication',
-    'UserProfile'
+    'UserProfile',
+    'Movie',
+    'MovieReviews',
 ]
 
 MIDDLEWARE = [
