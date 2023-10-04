ef651bd65606b48e3baef147b54f7987d4fbc8e5
Franz Genegobis <genegobis.franz@gmail.com>
2023-10-04 11:36:36 +0800

    Done UserProfile app.

--- UserProfile/models.py
+++ UserProfile/models.py
@@ -1,39 +1,17 @@
 from django.db import models
 
 from Authentication.models import User
+from MovieReviews.models import Review
+from SavedMovies.models import FavoriteMovies
+from SavedMovies.models import WatchList
+from SavedMovies.models import Watched
 
 
 # Create your models here.
 class Profile(models.Model):
     # Kulang pa ang user model.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    pass
-
-
-class FavoriteMovies(models.Model):
-    favoriteID = models.IntegerField(primary_key=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    # movieID = Foreign key sa list of movies.
-    pass
-
-
-class WatchList(models.Model):
-    watchlistID = models.IntegerField(primary_key=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    # movieID = Foreign key sa list of movies
-    pass
-
-
-class ReviewMade(models.Model):
-    # reviewMade =  primary key sa reviewMade nga table.
-
-    """ userID = Maybe I should get the userID  as foreign key for me to know what are the movies or review he had
-    made."""
-
-    """" reviewID = for us to know nga unsa nga movie ang ge review ani nga user, 
-    kuhaon nako ang foreign key aning reveiwID. """
-
-    """In summary, gamit ang userID og ang reviewID, mahibawan nako sa review nga table if kani nga foreign 
-    keys nag review ngadto sa review."""
-
-    pass
+    userID = models.OneToOneField(User, on_delete=models.CASCADE)
+    favoriteID = models.ForeignKey(FavoriteMovies, on_delete=models.CASCADE)
+    watchlistID = models.ForeignKey(WatchList, on_delete=models.CASCADE)
+    watched = models.ForeignKey(Watched, on_delete=models.CASCADE)
+    reviewID = models.ForeignKey(Review, on_delete=models.CASCADE)
