0e0e75a582752c61c2f8234f1cfa2bc96c0883dc
Jess Oliver Zoilo <zjessoliver@gmail.com>
2023-10-04 10:32:36 +0800

    added temporary models

--- MovieReviews/models.py
+++ MovieReviews/models.py
@@ -2,24 +2,7 @@ from django.db import models
 # import models from other apps
 
 # Create your models here.
-
-#temporary model
-class User(models.Model):
-    # Add any additional user attributes here
-    username = models.CharField(max_length=20)
-    pass
-
-#temporary model
-class Movie(models.Model):
-    # Movie attributes
-    title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=100)
-    release_date = models.DateField()
-
-    def __str__(self):
-        return self.title
-
-
+from Authentication.models import User
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
